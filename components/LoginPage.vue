20e862a0b4b82136ca7fd3da13f65a286672fa8f
Jeriks1223 <jerectamz@gmail.com>
2022-10-14 07:25:28 +0800

    Commit

---
+++ components/LoginPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled</title>
+  
+</head>
+
+<body>
+    <div class="body" style="padding-top: 150px;">
+        <div class="border rounded shadow login-center">
+            <form style="padding-top: 32px;">
+                <div class="d-flex justify-content-center form-group mb-3"><input class="form-control" type="text" style="width: 200px;" placeholder="Username / Email"></div>
+                <div class="text-center form-group mb-3"><img class="img-fluid" style="width: 100px;" src="assets/img/default.png"></div>
+                <div class="d-flex justify-content-center form-group mb-3"><input class="form-control" type="password" style="width: 200px;" placeholder="Password"></div>
+                <div class="d-flex justify-content-center form-group mb-3"><button class="btn btn-dark" style="width: 200px;" type="button">Login</button></div>
+            </form>
+        </div>
+    </div>
+    <!--<script src="assets/bootstrap/js/bootstrap.min.js"></script>-->
+</body>
+
+</html>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <LoginPage/>
+</template>
+
+<script>
+
+import LoginPage from '../components/LoginPage.vue';
+export default {
+    name: "IndexPage",
+    components: {LoginPage }
+}
+</script>
